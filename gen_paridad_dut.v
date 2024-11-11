d6469970a71784ae04fd947fe058c9e368d2719b
Daniel De La O R <daniel.delao@ucr.ac.cr>
2024-11-10 22:19:12 -0600

    agregando archivos corregidos y un shift register
    Signed-off-by: Daniel De La O R <daniel.delao@ucr.ac.cr>

--- gen_paridad_dut.v
+++ gen_paridad_dut.v
@@ -1,6 +1,6 @@
 module gen_paridad (
-    input [6:0] data,   //datos sobre los que se generará el bit de paridad
-    input parimpar,     //entrada que determina el tipo de paridad deseada
+    input [6:0] data,   // datos sobre los que se generará el bit de paridad
+    input parimpar,     // entrada que determina el tipo de paridad deseada
     output wire paridad // salida que contiene el bit de paridad calculado
 );
     assign paridad = (^data) ^ parimpar;
