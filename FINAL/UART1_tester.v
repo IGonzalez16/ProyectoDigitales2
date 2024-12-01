e5f4e67b65048344f13e69e3103cff915afccbfa
Daniel De La O R <daniel.delao@ucr.ac.cr>
2024-11-30 23:20:45 -0600

    Subiendo tester arreglado
    Signed-off-by: Daniel De La O R <daniel.delao@ucr.ac.cr>

--- FINAL/UART1_tester.v
+++ FINAL/UART1_tester.v
@@ -4,7 +4,6 @@ module UART1_tester(
     output reg idle_bit,
     output reg start_bit,
     output reg [7:0] tx1,
-    output reg parity_bit,
     output reg stop_bit,
     input wire serial_out
 );
@@ -19,18 +18,32 @@ module UART1_tester(
         idle_bit = 1;
         start_bit = 1;
         tx1 = 8'b00000000;
-        parity_bit = 0;
-        stop_bit = 1;
+        stop_bit = 0;
 
         #10 rst = 0;
 
         #10 idle_bit = 0; 
-        #10 start_bit = 0; 
-        #10 start_bit = 1; tx1 = 8'b10101100; 
-        #80 parity_bit = 1; tx1 = 8'b0; 
-        #10 stop_bit = 1; parity_bit = 0;
-        #10 idle_bit = 1; 
+        start_bit = 0; 
+        #10 start_bit = 1; tx1 = 8'b10100100; 
+        #90 tx1 = 8'b0; 
+        #10 stop_bit = 1; 
+        #10 idle_bit = 1; stop_bit = 0; 
         #200 $finish;
     end
 
 endmodule
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
